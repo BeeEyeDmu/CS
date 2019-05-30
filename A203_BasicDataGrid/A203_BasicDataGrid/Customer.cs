93ea115cc9cecb06e7df1b49ddaed53983e36400
BeeEye Dmu <bikang@konyang.ac.kr>
2019-05-30 19:44:23 +0900

    A203_BasicDataGrid Added

---
+++ A203_BasicDataGrid/A203_BasicDataGrid/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A203_BasicDataGrid
+{
+  class Customer
+  {
+    public int UserID { get; set; }
+    public string UserName { get; set; }
+    public string UserSex { get; set; }
+    public string UserPhone { get; set; }
+  }
+}
---
+++ A203_BasicDataGrid/A203_BasicDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace A203_BasicDataGrid
+{
+  /// <summary>
+  /// MainWindow.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      List<Customer> customers = new List<Customer>();
+      customers.Add(new Customer { UserID = 190001, UserName = "홍길동", UserSex = "남", UserPhone = "010-1236-5678" });
+      customers.Add(new Customer { UserID = 190002, UserName = "성춘향", UserSex = "여", UserPhone = "010-2145-4512" });
+      dataGrid.ItemsSource = customers;
+    }
+  }
+}
